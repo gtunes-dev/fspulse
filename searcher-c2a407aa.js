0ba4c9da6fce831ae49bb87f517003224a2fd6a3
gtunes-dev <gtunes-dev@users.noreply.github.com>
2026-03-28 18:14:16 +0000

    deploy: e6fb39f21a8d8cc90062a8767f038fdd7af7fc48

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1f590892.js',
+                path_to_root + 'searchindex-ac2fac39.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
